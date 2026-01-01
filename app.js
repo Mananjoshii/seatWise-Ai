6ea348a3596ae83706cfb92f519819dba4571a38
Mananjoshii <joshimanan074@gmail.com>
2026-01-01 23:56:17 +0530

    project setup with express, ejs, static files

---
+++ app.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// view engine
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+// middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+
+// routes
+const indexRoutes = require("./routes/index.routes");
+app.use("/", indexRoutes);
+
+// server
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
