53e45f1bb0a797b6636698c638f3449018f693c0
Mananjoshii <joshimanan074@gmail.com>
2026-01-06 01:23:44 +0530

    V1.0

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const PORT = process.env.PORT || 3000;
 // view engine
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.set("trust proxy", 1);
 
 // middleware
 app.use(express.urlencoded({ extended: true }));
